fc9f71d8251a876dea9429422aa56bbcade7654c
Bruno Silva <bruno.gomes@belezanaweb.com.br>
2021-11-24 15:15:55 -0300

    refactor: improve ui with style and features

--- static/app.js
+++ static/app.js
@@ -3,22 +3,140 @@ const Controller = {
     ev.preventDefault();
     const form = document.getElementById("form");
     const data = Object.fromEntries(new FormData(form));
+    Controller.cleanResults();
+    if ((data.query || "").length === 0) {
+      return;
+    }
     const response = fetch(`/search?q=${data.query}`).then((response) => {
       response.json().then((results) => {
-        Controller.updateTable(results);
+        Controller.updateSearchResult(results);
       });
     });
   },
 
-  updateTable: (results) => {
-    const table = document.getElementById("table-body");
-    const rows = [];
-    for (let result of results) {
-      rows.push(`<tr><td>${result}</td><tr/>`);
-    }
-    table.innerHTML = rows;
+  getBookByTitle: (id) => {
+    const form = document.getElementById(id);
+    Controller.cleanResults();
+    const response = fetch(`/book?title=${form.title}`).then((response) => {
+      response.json().then((results) => {
+        Controller.updateBookContent(results);
+      });
+    });
   },
+
+  updateSearchResult: (results) => {
+    const booksPlace = document.getElementById("books_place");
+    const quotesPlace = document.getElementById("quotes_place");
+    const booksCards = [];
+    const quotesCards = [];
+
+    if ((results.Books || []).length > 0) {
+      for (let i = 0; i < results.Books.length; i++) {
+        const b = results.Books[i]
+        const content = b.Chapters[0].Content
+        booksCards.push(`
+          <div class="card">
+            <div class="card-header">
+              ${b.Title}
+            </div>
+            <div class="card-body">
+              <blockquote class="blockquote mb-0">
+                <pre>${content.substr(0, 200) + "..."}</pre>
+                <footer class="blockquote-footer">${b.Chapters[0].Name} <cite title="Book Name">${b.Title}</cite></footer>
+              </blockquote>
+              <button class="card-link btn btn-secondary get-book" id="get-book-${i}" title="${b.Title}" onclick="Controller.getBookByTitle('get-book-${i}')">Read Now</button>
+            </div>
+          </div>
+        `);
+      }
+    }
+
+    if ((results.Quotes || []).length > 0) {
+      quotesCards.push(`<div class="list-group">`);
+      for (let q of results.Quotes) {
+        quotesCards.push(`
+            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start" 
+                data-toggle="modal" data-target="#quote-modal" data-content="${q.Chapters[0].Content}">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">${q.Title}</h5>
+              </div>
+              <p class="mb-1">${q.Chapters[0].Content.substr(0, 200) + "..."}</p>
+            </a>`)
+        for (let it = 1; it < 5; it++) {
+          if (!q.Chapters[it])
+            break
+          quotesCards.push(`
+            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start" data-toggle="modal" data-target="#quote-modal" data-content="${q.Chapters[it].Content}">
+              <small class="text-muted">${q.Chapters[it].Content}</small>
+            </a>
+          `)
+        }
+        quotesCards.push(`
+            <p class="list-group-item list-group-item-action flex-column align-items-start">
+              <small class="text-muted">Read more...</small>
+            </p>
+          `)
+      }
+      quotesCards.push(`</div>`);
+    }
+    if (booksCards.length > 0) {
+      booksPlace.innerHTML = booksCards.join('');
+      booksPlace.parentElement.style.visibility = "visible";
+    }
+    if (quotesCards.length > 0) {
+      quotesPlace.innerHTML = quotesCards.join('');
+      quotesPlace.parentElement.style.visibility = "visible";
+    }
+  },
+
+  cleanResults: () => {
+    const booksPlace = document.getElementById("books_place");
+    const quotesPlace = document.getElementById("quotes_place");
+    booksPlace.innerHTML = '';
+    quotesPlace.innerHTML = '';
+    booksPlace.parentElement.style.visibility = "hidden";
+    quotesPlace.parentElement.style.visibility = "hidden";
+  },
+
+  updateBookContent: (results) => {
+    const booksPlace = document.getElementById("books_place");
+    const booksCards = [];
+
+    if (results) {
+      for (let b of [results]) {
+        booksCards.push(`<h5 class="card-title">${b.Title}</h5>`);
+        for (let c of b.Chapters) {
+          booksCards.push(`
+            <div class="card" style="width: 200rem;">
+              <div class="card-body">
+                <pre class="card-text" style="max-width: 200rem">${c.Content}</pre>
+              </div>
+            </div>
+          `)
+        }
+      }
+    }
+
+    booksPlace.innerHTML = booksCards.join('');
+    booksPlace.parentElement.style.visibility = "visible";
+  }
 };
 
 const form = document.getElementById("form");
 form.addEventListener("submit", Controller.search);
+
+const buttons = document.getElementsByClassName("get-book");
+for (let button of buttons) {
+  button.addEventListener("click", Controller.getBookByTitle);
+}
+
+$(function() {
+
+  $('#quote-modal').on('show.bs.modal', function (event) {
+    const component = $(event.relatedTarget)
+    const quoteContent = component.data('content')
+    const modal = $(this)
+    modal.find('.modal-body pre').text(quoteContent)
+  });
+
+});
