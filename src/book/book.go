3d2c44f2f82a1e293bd919a81d18a7c1ce1761a3
Bruno Silva <bruno.gomes@belezanaweb.com.br>
2021-12-01 12:14:18 -0300

    refactor: isolate indexer code

--- src/book/book.go
+++ src/book/book.go
@@ -3,12 +3,6 @@ package book
 import (
 	"fmt"
 	"github.com/blevesearch/bleve/v2"
-	"github.com/blevesearch/bleve/v2/analysis/analyzer/custom"
-	"github.com/blevesearch/bleve/v2/analysis/lang/en"
-	"github.com/blevesearch/bleve/v2/analysis/token/lowercase"
-	"github.com/blevesearch/bleve/v2/analysis/token/porter"
-	"github.com/blevesearch/bleve/v2/analysis/tokenizer/unicode"
-	"github.com/blevesearch/bleve/v2/mapping"
 	"index/suffixarray"
 	"io/ioutil"
 	"regexp"
@@ -41,21 +35,16 @@ func Parse(filename string) ([]Book, *bleve.Index, *bleve.Index, error) {
 	contentByBook := titleSplitPattern.Split(fullContent, -1)
 
 	books := []Book{}
-	bookMapping := buildBookIndexMapping()
-	chapterMapping := buildChapterIndexMapping()
-	booksIndex, _ := bleve.NewMemOnly(bookMapping)
-	chapterIndex, _ := bleve.NewMemOnly(chapterMapping)
 
 	for _, content := range contentByBook[:len(contentByBook)-1] {
 		newBook := parseBook(content)
-		booksIndex.Index(strings.TrimSpace(newBook.Title), Book{Title: newBook.Title})
 		for _, c := range newBook.Chapters {
 			c.Title = newBook.Title
-			chapterIndex.Index(newBook.Title+c.Name, c)
 		}
 		books = append(books, newBook)
 	}
-	return books, &booksIndex, &chapterIndex, nil
+	booksIndex, chapterIndex := IndexDocuments(books)
+	return books, booksIndex, chapterIndex, nil
 }
 
 func parseBook(bookContent string) Book {
@@ -66,43 +55,4 @@ func parseBook(bookContent string) Book {
 		Title:    titleAndContent[0],
 		Chapters: chapters,
 	}
-}
-
-func buildBookIndexMapping() mapping.IndexMapping {
-	titleFieldMapping := bleve.NewTextFieldMapping()
-	bookMapping := bleve.NewDocumentMapping()
-	bookMapping.AddFieldMappingsAt("Title", titleFieldMapping)
-	indexMapping := bleve.NewIndexMapping()
-	indexMapping.AddDocumentMapping("book", bookMapping)
-	indexMapping.DefaultMapping = bookMapping
-	return indexMapping
-}
-
-func buildChapterIndexMapping() mapping.IndexMapping {
-	englishTextFieldMapping := bleve.NewTextFieldMapping()
-	englishTextFieldMapping.Analyzer = en.AnalyzerName
-	englishTextFieldMapping.IncludeTermVectors = true
-
-	contentFieldMapping := bleve.NewTextFieldMapping()
-	contentFieldMapping.IncludeTermVectors = true
-	contentFieldMapping.Analyzer = "enWithStopWords"
-
-	chapterMapping := bleve.NewDocumentMapping()
-	chapterMapping.AddFieldMappingsAt("Title", englishTextFieldMapping)
-	chapterMapping.AddFieldMappingsAt("Content", contentFieldMapping)
-
-	indexMapping := bleve.NewIndexMapping()
-	indexMapping.AddDocumentMapping("bookChapter", chapterMapping)
-	indexMapping.AddCustomAnalyzer("enWithStopWords",
-		map[string]interface{}{
-			"type":      custom.Name,
-			"tokenizer": unicode.Name,
-			"token_filters": []string{
-				en.PossessiveName,
-				lowercase.Name,
-				porter.Name,
-			},
-		})
-	indexMapping.DefaultMapping = chapterMapping
-	return indexMapping
 }
---
+++ src/book/indexer.go
@@ -0,0 +1,74 @@
+package book
+
+import (
+	"github.com/blevesearch/bleve/v2"
+	"github.com/blevesearch/bleve/v2/analysis/analyzer/custom"
+	"github.com/blevesearch/bleve/v2/analysis/lang/en"
+	"github.com/blevesearch/bleve/v2/analysis/token/lowercase"
+	"github.com/blevesearch/bleve/v2/analysis/token/porter"
+	"github.com/blevesearch/bleve/v2/analysis/tokenizer/unicode"
+	"github.com/blevesearch/bleve/v2/mapping"
+	"strings"
+)
+
+func IndexDocuments(books []Book) (*bleve.Index, *bleve.Index) {
+	bookMapping := buildBookIndexMapping()
+	chapterMapping := buildChapterIndexMapping()
+	booksIndex, _ := bleve.NewMemOnly(bookMapping)
+	chapterIndex, _ := bleve.NewMemOnly(chapterMapping)
+
+	bookBatch := booksIndex.NewBatch()
+	chapterBatch := chapterIndex.NewBatch()
+	for _, b := range books {
+		bookBatch.Index(strings.TrimSpace(b.Title), Book{Title: b.Title})
+		for _, c := range b.Chapters {
+			c.Title = b.Title
+			chapterBatch.Index(b.Title+c.Name, c)
+			if chapterBatch.Size() > 500 {
+				chapterIndex.Batch(chapterBatch)
+				chapterBatch = chapterIndex.NewBatch()
+			}
+		}
+	}
+	booksIndex.Batch(bookBatch)
+	return &booksIndex, &chapterIndex
+}
+
+func buildBookIndexMapping() mapping.IndexMapping {
+	titleFieldMapping := bleve.NewTextFieldMapping()
+	bookMapping := bleve.NewDocumentMapping()
+	bookMapping.AddFieldMappingsAt("Title", titleFieldMapping)
+	indexMapping := bleve.NewIndexMapping()
+	indexMapping.AddDocumentMapping("book", bookMapping)
+	indexMapping.DefaultMapping = bookMapping
+	return indexMapping
+}
+
+func buildChapterIndexMapping() mapping.IndexMapping {
+	englishTextFieldMapping := bleve.NewTextFieldMapping()
+	englishTextFieldMapping.Analyzer = en.AnalyzerName
+	englishTextFieldMapping.IncludeTermVectors = true
+
+	contentFieldMapping := bleve.NewTextFieldMapping()
+	contentFieldMapping.IncludeTermVectors = true
+	contentFieldMapping.Analyzer = "enWithStopWords"
+
+	chapterMapping := bleve.NewDocumentMapping()
+	chapterMapping.AddFieldMappingsAt("Title", englishTextFieldMapping)
+	chapterMapping.AddFieldMappingsAt("Content", contentFieldMapping)
+
+	indexMapping := bleve.NewIndexMapping()
+	indexMapping.AddDocumentMapping("bookChapter", chapterMapping)
+	indexMapping.AddCustomAnalyzer("enWithStopWords",
+		map[string]interface{}{
+			"type":      custom.Name,
+			"tokenizer": unicode.Name,
+			"token_filters": []string{
+				en.PossessiveName,
+				lowercase.Name,
+				porter.Name,
+			},
+		})
+	indexMapping.DefaultMapping = chapterMapping
+	return indexMapping
+}
